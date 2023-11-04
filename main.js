117a43d7c37d3855078f51eb5dacfd44c847247c
thelastenvoy <satvikbatra@gmail.com>
2023-11-04 15:26:36 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-const fs = require('fs');
-const https = require('https');  // Note: I'm using 'https' here because the given test URL uses HTTPS
+// main.js
+const fs = require('fs').promises;
+const https = require('https'); // Use 'https' for 'https://' URLs
 
+// Fetch data from API
 function fetchFromAPI(url) {
   return new Promise((resolve, reject) => {
     https.get(url, (res) => {
+      if (res.statusCode < 200 || res.statusCode >= 300) {
+        return reject(new Error('statusCode=' + res.statusCode));
+      }
       let data = '';
 
       res.on('data', (chunk) => {
@@ -17,30 +22,35 @@ function fetchFromAPI(url) {
           reject(e);
         }
       });
-    }).on('error', (error) => {
-      reject(error);
-    });
+    }).on('error', reject);
   });
 }
 
-function saveToFile(filePath, data) {
-  return new Promise((resolve, reject) => {
-    fs.writeFile(filePath, JSON.stringify(data), (err) => {
-      if (err) reject(err);
-      else resolve();
-    });
-  });
+// Save data to file
+async function saveToFile(filePath, data) {
+  await fs.writeFile(filePath, JSON.stringify(data, null, 2));
 }
 
-module.exports = function fetchAndSave(url, filePath) {
-  return fetchFromAPI(url)
-    .then((data) => saveToFile(filePath, data));
-};
-
-// Calling fetchFromAPI and saveToFile with command-line arguments
-const [,, apiUrl, outputFilePath] = process.argv;
-if (apiUrl && outputFilePath) {
-  fetchAndSave(apiUrl, outputFilePath)
-    .then(() => console.log('Data fetched and saved successfully.'))
-    .catch(error => console.error('Error:', error));
+// Process the command-line arguments and perform the operations
+async function main(url, filePath) {
+  try {
+    const data = await fetchFromAPI(url);
+    await saveToFile(filePath, data);
+    console.log(`Data from ${url} has been saved to ${filePath}`);
+  } catch (error) {
+    console.error('Error:', error.message);
+    process.exit(1); // Exit the process with an error code
+  }
 }
+
+// Check if we have the right amount of arguments
+if (process.argv.length !== 4) {
+  console.error('Usage: node main.js <API_URL> <FILE_PATH>');
+  process.exit(1);
+}
+
+// Get command-line arguments
+const [, , url, filePath] = process.argv;
+
+// Call the main function with the command-line arguments
+main(url, filePath);
